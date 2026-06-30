6b82f61c6a8c35ba665467d4f2ca55e5fae14a55
Zzukov <hatsawat.456@gmail.com>
2026-06-30 16:37:20 +0700

    "docs: add production pipeline diagrams (week 2)"

--- obj/Debug/net9.0/MyNewGame.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyNewGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyNewGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffb3342494803a8df1678819f47f0bdc07227dee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcd0cd1ff23ec138aaebffd453404c54ee370fbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyNewGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyNewGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
